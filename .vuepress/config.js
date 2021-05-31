edfeade37ebf5ee4d2c6c8a22689bf05070949c6
zhaoyong <zhaoyongajmx@163.com>
2021-05-31 16:51:37 +0800

    添加笔记

---
+++ .vuepress/config.js
@@ -0,0 +1,91 @@
+module.exports = {
+    base: "/blog/",
+    title: "前端笔记",
+    description: "基础面试笔记",
+    themeConfig: {
+        nav: [
+          { text: 'github', link: 'https://github.com/wxw007/learning-note' },
+        ],
+        sidebarDepth: 2,
+        sidebar:[
+            '/html',
+            '/css',
+            '/js',
+            '/algorithm',
+            '/vue',
+            '/brower',
+            '/webpack',
+            '/optimization',
+            '/git'
+        ],
+        // sidebar: [
+        //     {
+        //         title: 'HTML',
+        //         children: [
+        //             '/html'
+        //         ]
+        //     },
+        //     {
+        //         title: 'CSS',
+        //         children: [
+        //             '/css'
+        //         ]
+        //     },
+        //     {
+        //         title: 'JS基础',
+        //         children: [
+        //             '/js'
+        //         ]
+        //     },
+        //     {
+        //         title: 'VUE',
+        //         children: [
+        //             '/vue'
+        //         ]
+        //     },
+        //     {
+        //         title: 'JS设计模式',
+        //         children: [
+        //             '/design-pattern'
+        //         ]
+        //     },
+        //     {
+        //         title: 'JS算法',
+        //         children: [
+        //             '/algorithm'
+        //         ]
+        //     },
+        //     {
+        //         title: 'webpack',
+        //         children: [
+        //             '/webpack'
+        //         ]
+        //     },
+        //     {
+        //         title: 'node.js',
+        //         children: [
+        //             '/node'
+        //         ]
+        //     },
+        //     {
+        //         title: '浏览器相关',
+        //         children: [
+        //             '/brower'
+        //         ]
+        //     },
+        //     {
+        //         title: '性能优化',
+        //         children: [
+        //             '/optimization'
+        //         ]
+        //     },
+        //     {
+        //         title: 'git',
+        //         children: [
+        //             '/git'
+        //         ]
+        //     },
+           
+        //   ]
+      }
+}
